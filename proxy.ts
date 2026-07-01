9c864af74db1150456572ff9d7904d112563a724
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-07-01 21:30:51 +0530

    next.js setup

---
+++ proxy.ts
@@ -0,0 +1,24 @@
+import { createRouteMatcher, clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
+
+const isPublicRoute = createRouteMatcher([
+    "/sign-in",
+    "/sign-up",
+    "/",
+    "/home"
+])
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    // Always run for Clerk-specific frontend API routes
+    '/__clerk/(.*)',
+  ],
+}
